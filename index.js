0d4102e983a73f427f71b81390efbf6929130fca
Avishekdevnath <avishekdevnath@gmail.com>
2022-02-24 02:24:17 +0600

    A

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ernrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ernrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
+async function run() {
 
   try {
     await client.connect();
     console.log("database connected");
     const database = client.db('foodmart_shop');
     const productCollection = database.collection('products')
+    const productsNewCollection = database.collection('productsNew')
     const blogCollection = database.collection('blogs')
     const reviewCollection = database.collection('reviews')
 
@@ -26,19 +28,41 @@ async function run(){
       const cursor = productCollection.find({})
       const products = await cursor.toArray();
       res.send(products);
-  })
+    })
+    app.get('/productsNew', async (req, res) => {
+      const cursor = productsNewCollection.find({})
+      const products = await cursor.toArray();
+      res.send(products);
+    })
     app.get('/blogs', async (req, res) => {
       const cursor = blogCollection.find({})
       const blogs = await cursor.toArray();
       res.send(blogs);
-  })
+    })
+
+    // app.post('/blogs', async (req, res) => {
+    //   const blog = req.body;
+    //   console.log(blog);
+    //   const result = await blogCollection.insertOne(blog);
+    //   res.json(result);
+    // })
+    app.post('/blogs', async (req, res) => {
+      const blog = req.body;
+      console.log(blog)
+      const result = await blogCollection.insertOne(blog);
+      console.log(result);
+      res.json(result);
+    });
+
+
+
     app.get('/reviews', async (req, res) => {
       const cursor = reviewCollection.find({})
       const reviews = await cursor.toArray();
       res.send(reviews);
-  })
+    })
 
-  } 
+  }
   finally {
     // await client.close();
   }
@@ -54,5 +78,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(` listening at ${port}`)
+  console.log(`listening at ${port}`)
 })
